7c4d9f809eda23fb270be465c8a630ab21348143
binay kumar prasad <binaykumar863731@gmail.com>
2026-04-30 23:17:23 +0530

    Require .com emails in signup validation

--- frontend/src/lib/signupValidation.js
+++ frontend/src/lib/signupValidation.js
@@ -1,6 +1,6 @@
 import { getPlacementYearOptions } from "./studentOptions";
 
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+const emailRegex = /^[^\s@]+@[^\s@]+\.com$/i;
 const phoneRegex = /^\d{10}$/;
 const nameRegex = /^[A-Za-z][A-Za-z\s.'-]*$/;
 const rollNoRegex = /^[A-Za-z0-9/-]+$/;
